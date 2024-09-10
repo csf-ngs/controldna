59675fa365f4700072ee822600b9b125c359a76b
Ido Tamir <ido.tamir@vbcf.ac.at>
2024-09-10 17:00:07 +0200

    R17038

---
+++ examples/R17038/align_R17038.sh
@@ -0,0 +1,5 @@
+sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R17038_aviti_25M /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R17038/R17038_aviti.csv GRCh38 R17038_aviti_25M  0
+sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R17038_novax_25M /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R17038/R17038_novax.csv GRCh38 R17038_novax_25M  0
+
+
+#/groups/vbcf-ngs/misc/reports/other/analysis/ido/watchmaker_dna/20240909
---
+++ examples/R17038/gen_csv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+../../scripts/create_samples_csv.py --inputfolder /scratch/csfs/ido/tmp/undemultiplexed/22MLNKLT3_8_R17384_20240718/demultiplexed  --outputpath R17038_novax.csv --subsample 25Mf
+../../scripts/create_samples_csv.py --inputfolder /scratch/csfs/ido/tmp/undemultiplexed/2348627951_1_R17384_20240804/demultiplexed  --outputpath R17038_aviti.csv --subsample 25Mf
