70a9e5fcaca72b9eaa6202131ad6826a76e4970d
Ido Tamir <ido.tamir@vbcf.ac.at>
2025-07-14 10:52:10 +0200

    R18913

---
+++ examples/R18913/align_R18913.sh
@@ -0,0 +1,3 @@
+sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R18913 /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R18913/R18913.csv GRCh38 R18913  0
+
+
