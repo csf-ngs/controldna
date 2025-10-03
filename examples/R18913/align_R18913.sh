1a4dc9bf924b8e42cfefcb7f3802f7c4b0f80c26
Ido Tamir <ido.tamir@vbcf.ac.at>
2025-10-03 10:40:20 +0200

    changes

--- examples/R18913/align_R18913.sh
+++ examples/R18913/align_R18913.sh
@@ -1,3 +1,3 @@
-sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R18913 /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R18913/R18913.csv GRCh38 R18913  0
+#sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R18913a /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R18913/R18913.csv GRCh38 R18913  0
 
-
+sbatch --qos=long --time=4-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R18913b /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R18913/R18913.csv GRCh38 R18913  0
---
+++ examples/R19507/align_R19507.sh
@@ -0,0 +1,3 @@
+sbatch --qos=medium --time=2-00:00:00 /users/ido.tamir/work/pipelines/nf-core-controldna/run_cluster.sh R19507 /users/ido.tamir/work/pipelines/nf-core-controldna/examples/R19507/R19507.csv dm6 R19507  0
+
+
